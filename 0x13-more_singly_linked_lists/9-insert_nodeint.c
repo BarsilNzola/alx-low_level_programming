a43ea9b7848e0bb8b3e9c9c91cebe5dd37ba1174
BarsilNzola <barsiljohn@gmail.com>
2023-05-02 04:00:09 -0700

    l

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,39 +1,39 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position.
- * @head: Pointer to a pointer to the first node of the list.
- * @idx: Index of the list where the new node should be added.
- * @n: Data for the new node.
- *
- * Return: Address of the new node, or NULL if it failed.
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to the first node of the listint_t list
+ * @idx: index position of the node to be inserted
+ * @n: value of the new node
+ * Return: the listint_t list after the new node has been inserted
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *temp_node;
-    unsigned int i;
+	listint_t *new_node, *node_to_insert_after;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return (NULL);
-
-    if (idx == 0)
-        return (add_nodeint(head, n));
-
-    temp_node = *head;
-    for (i = 0; i < idx - 1 && temp_node != NULL; i++)
-        temp_node = temp_node->next;
-
-    if (temp_node == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->next = temp_node->next;
-    temp_node->next = new_node;
-
-    return (new_node);
+	node_to_insert_after = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = node_to_insert_after;
+		*head = new_node;
+		return (new_node);
+	}
+	while (i < idx - 1)
+	{
+		if (!node_to_insert_after)
+		{
+			free(new_node);
+			return (NULL);
+		}
+node_to_insert_after = node_to_insert_after->next;
+i++;
+	}
+	new_node->next = node_to_insert_after->next;
+	node_to_insert_after->next = new_node;
+	return (new_node);
 }
-
